436a6a5a575206cbdb64fa5531469d73e825c45a
Isaac Sarte <aizakku09@gmail.com>
2021-12-11 11:48:37 +0800

    navbar changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,6 +4,9 @@ import { NavLink } from 'react-router-dom';
 import styles from './navbar.module.css';
 
 // Icons
+import HomeIcon from '@material-ui/icons/Home';
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
+import VerifiedUserIcon from '@material-ui/icons/VerifiedUser';
 import LooksOneOutlinedIcon from '@material-ui/icons/LooksOneOutlined';
 
 const Navbar = () => {
@@ -20,14 +23,17 @@ const Navbar = () => {
             <ul>
 
                 <li className={styles.link}>
+                    <HomeIcon className={styles.navLogo} />
                     <NavLink to="/" exact>Home</NavLink>
                 </li>
 
                 <li>
+                    <SupervisorAccountIcon className={styles.navLogo} />
                     <NavLink to="/admin" exact>Admin</NavLink>
                 </li>
 
                 <li>
+                    <VerifiedUserIcon className={styles.navLogo} />
                     <NavLink to="/client" exact>Client</NavLink>
                 </li>
 
