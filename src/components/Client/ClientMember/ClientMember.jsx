5e3ebc004bd4d93e04a905b6c672eb274a3acf1b
Isaac Sarte <aizakku09@gmail.com>
2021-12-11 11:36:49 +0800

    client sign in styling done

--- src/components/Client/ClientMember/ClientMember.jsx
+++ src/components/Client/ClientMember/ClientMember.jsx
@@ -1,12 +1,15 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 
 // CSS
 import styles from './clientMember.module.css'
 
+// Framer Motion
+import { motion, useAnimation } from 'framer-motion/dist/es/';
+
 const ClientMember = (props) => {
 
     // Destructured Properties
-    const {setClientActive,setBankMember} = props;
+    const { setClientActive, setBankMember } = props;
     const memberList = JSON.parse(localStorage.getItem("memberList"));
 
     // States
@@ -22,7 +25,7 @@ const ClientMember = (props) => {
     const handleSignIn = () => {
 
         let target = memberList.find(obj => obj.email === userEmail || obj.username === userEmail);
-        if(target === undefined) {
+        if (target === undefined) {
             setErrorUserEmail("User does not exist");
         } else {
             if (target.password !== password) {
@@ -45,38 +48,64 @@ const ClientMember = (props) => {
         <div className={styles.container}>
 
             <section className={styles.design}>
-                Log In as Ichibank Member
+                {/* Image Goes Here */}
             </section>
 
             <section className={styles.content}>
-                
-                <label>
-                    Username | Email
-                    <input 
-                        type="text" 
+
+                <motion.h1
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 1, delay: 1 }}
+                >
+                    Welcome to IchiBank! Your All in One Banking Needs<br /><br />
+                    <span>Sign In</span> Now
+                </motion.h1>
+
+                <motion.label
+                    className={styles.form_label}
+                    initial={{ opacity: 0, x: '-15vw' }}
+                    animate={{ opacity: 1, x: '0vw' }}
+                    transition={{ duration: 1, delay: 0 }}
+                >
+                    Username | Email <br /><br />
+                    <input
+                        className={styles.form_input}
+                        type="text"
+                        placeholder="Enter your username or email"
                         value={userEmail}
-                        onChange={(e) => {setUserEmail(e.target.value.trim()); setErrorUserEmail("");}}
+                        onChange={(e) => { setUserEmail(e.target.value.trim()); setErrorUserEmail(""); }}
                         required
-                        />
+                    />
                     <h5>{errorUserEmail}</h5>
-                </label>
+                </motion.label>
 
-                <label>
-                    Password
-                    <input 
-                        type="password" 
+                <motion.label
+                    className={styles.form_label}
+                    initial={{ opacity: 0, x: '15vw' }}
+                    animate={{ opacity: 1, x: '0vw' }}
+                    transition={{ duration: 1, delay: 0 }}
+                >
+                    Password <br /><br />
+                    <input
+                        className={styles.form_input}
+                        type="password"
+                        placeholder="Enter your password"
                         value={password}
-                        onChange={(e) => {setPassword(e.target.value); setErrorPassword("");}}
+                        onChange={(e) => { setPassword(e.target.value); setErrorPassword(""); }}
                         required
-                        />
+                    />
                     <h5>{errorPassword}</h5>
-                </label>
+                </motion.label>
 
-                <button onClick={handleSignIn}>Sign In</button>
+                <button
+                    className={styles.form_input_btn}
+                    onClick={handleSignIn}
+                >Sign In</button>
 
                 <p>
-                    Not an IchiBank Member? Sign In as 
-                    &nbsp; 
+                    Not an IchiBank Member? Sign In as
+                    &nbsp;
                     <span
                         onClick={handleGuestMember}>
                         Guest
@@ -86,7 +115,7 @@ const ClientMember = (props) => {
             </section>
 
 
-            
+
             {/* <h1>You are an IchiBank Member</h1>
 
             <span>Not an Ichibank Member?</span>
